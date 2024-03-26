ba5ece74a278f565d722e8a6eb924699a604f30b
yongruifang <yongruifang@qq.com>
2024-03-26 12:00:53 +0800

    fix(application):refactor

--- src/main/java/org/example/DemoApplication.java
+++ src/main/java/org/example/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("org.example.Mapper")
+@MapperScan("org.example.mapper")
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
