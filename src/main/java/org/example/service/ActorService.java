19ec02f786703d9383be410b32fc7f5e3515fd95
Fang Yongrui <1102649231@qq.com>
2024-03-26 14:29:24 +0800

    fix(wrong place): code put in wrong place, swap then works well

--- src/main/java/org/example/service/ActorService.java
+++ src/main/java/org/example/service/ActorService.java
@@ -1,29 +1,8 @@
 package org.example.service;
 
 import org.example.entity.Actor;
-import org.example.mapper.ActorMapper;
-import org.example.service.serviceImpl.ActorServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class ActorService implements ActorServiceImpl {
-    // storing the data temporary
-    List<Actor> actors = new ArrayList<>();
-    @Autowired
-    ActorMapper actorMapper;
-    @Override
-    public List<Actor> getAllActors() {
-        actors.clear();
-        for(Actor actor: actorMapper.selectList(null)){
-            actors.add(actor);
-            System.out.println(actor.toString());
-        }
-        System.out.println("+++++++++++++++++++");
-        System.out.println(actors);
-        return actors;
-    }
+public interface ActorService{
+    public List<Actor> getAllActors();
 }
--- src/main/java/org/example/service/serviceImpl/ActorServiceImpl.java
+++ src/main/java/org/example/service/serviceImpl/ActorServiceImpl.java
@@ -1,9 +1,29 @@
 package org.example.service.serviceImpl;
 
 import org.example.entity.Actor;
+import org.example.mapper.ActorMapper;
+import org.example.service.ActorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface ActorServiceImpl {
-    public List<Actor> getAllActors();
+@Service
+public class ActorServiceImpl implements ActorService {
+    // storing the data temporary
+    List<Actor> actors = new ArrayList<>();
+    @Autowired
+    ActorMapper actorMapper;
+    @Override
+    public List<Actor> getAllActors() {
+        actors.clear();
+        for(Actor actor: actorMapper.selectList(null)){
+            actors.add(actor);
+            System.out.println(actor.toString());
+        }
+        System.out.println("+++++++++++++++++++");
+        System.out.println(actors);
+        return actors;
+    }
 }
