3a82f84f58737d49fe88e1b592018ce6eea25a1f
Fang Yongrui <1102649231@qq.com>
2024-03-27 15:48:21 +0800

    feat(ui): layout -- login page

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,15 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+<q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <q-page class="flex flex-center bg-grey-2">
+        <q-card class="q-pa-md shadow-2 my_card" bordered>
+          <q-card-section class="text-center">
+            <div class="text-grey-9 text-h5 text-weight-bold">Sign in</div>
+            <div class="text-grey-8">Sign in below to access your account</div>
+          </q-card-section>
+          <q-card-section>
+            <q-input dense outlined v-model="email" label="Email Address"></q-input>
+            <q-input dense outlined class="q-mt-md" v-model="password" type="password" label="Password"></q-input>
+          </q-card-section>
+          <q-card-section>
+            <q-btn style="
+  border-radius: 8px;" color="dark" rounded size="md" label="Sign in" no-caps class="full-width"></q-btn>
+          </q-card-section>
+          <q-card-section class="text-center q-pt-none">
+            <div class="text-grey-8">Don't have an account yet?
+              <a href="#" class="text-dark text-weight-bold" style="text-decoration: none">Sign
+                up.</a></div>
+          </q-card-section>
+
+        </q-card>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script setup>
-defineOptions({
-  name: 'IndexPage'
-});
 </script>
