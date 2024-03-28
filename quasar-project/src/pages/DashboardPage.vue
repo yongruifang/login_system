376a5eb63af01e52e0450eb71b1c91c6dbafd1e7
Fang Yongrui <1102649231@qq.com>
2024-03-28 17:39:53 +0800

    redirect(logout) : add logout btn in home page

--- quasar-project/src/pages/DashboardPage.vue
+++ quasar-project/src/pages/DashboardPage.vue
@@ -2,11 +2,33 @@
   <q-page padding>
     Welcom to my Dashboard
     <!-- content -->
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn @click="logout" :ripple="false" color="secondary" label="Logout" no-caps />
+  </div>
   </q-page>
 </template>
 
-<script>
-export default {
-  // name: 'PageName',
+<script setup>
+import { useTokenStore } from 'src/stores/token-store';
+import { useRouter } from 'vue-router';
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar()
+
+const tokenStore = useTokenStore()
+const router = useRouter()
+
+const logout = () => {
+  //console.log('@TODO: clear Token')
+  tokenStore.clearToken()
+  //console.log('@TODO: redirect')
+  router.push("/")
+  // @TODO: Notify
+  $q.notify({
+    message: "log out successfully! ",
+    color: 'primary'
+  })
+  
 }
+
 </script>
