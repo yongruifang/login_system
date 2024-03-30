7c5b4be8155d58f9ce28e869103e410a53c39214
Fang Yongrui <1102649231@qq.com>
2024-03-30 21:56:37 +0800

    feat(logic_delete): by config
    need to alter table (I add a column named "is_deleted")

--- src/main/java/org/example/entity/Actor.java
+++ src/main/java/org/example/entity/Actor.java
@@ -1,9 +1,6 @@
 package org.example.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.*;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -23,4 +20,7 @@ public class Actor {
     @UpdateTimestamp
     @TableField("last_update")
     private Instant lastUpdate;
+    @TableLogic
+    @TableField("is_deleted")
+    private int isDeleted;
 }
